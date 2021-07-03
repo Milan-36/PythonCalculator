6ca8b3a4d757510a194f8f1bc995ba6f113414dc
MilanPatel3628 <34170004+MilanPatel3628@users.noreply.github.com>
2021-07-03 00:49:53 -0400

    Feature: Added Addition method

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,3 +1,6 @@
+def addition(a, b):
+    return a + b
+
 class Calculator:
     result = 0
 
@@ -6,3 +9,6 @@ class Calculator:
         self.result = x
         pass
 
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
