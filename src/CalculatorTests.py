e30490cd6f8de886c6f2c9c5bec8e26fea1e6dec
MilanPatel3628 <34170004+MilanPatel3628@users.noreply.github.com>
2021-07-05 19:41:41 -0400

    Task: Added picture of output of sqrt.

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -2,7 +2,7 @@ import unittest
 from Calculator import Calculator
 from CsvReader import CsvReader
 from StaticVariables import StaticVariables
-
+from pprint import pprint
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
@@ -54,6 +54,7 @@ class MyTestCase(unittest.TestCase):
         for row in test_square_root_data:
             self.assertAlmostEqual(self.calculator.sqrt(row[StaticVariables.val1]), float(row[StaticVariables.result]))
             self.assertAlmostEqual(self.calculator.result, float(row[StaticVariables.result]))
+            pprint(row)
 
 if __name__ == '__main__':
     unittest.main()
